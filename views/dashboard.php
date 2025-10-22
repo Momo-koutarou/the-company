437eef8267ec5c7eb96740ab443439e779268d17
Momo Hayamizu <momohayamizu@MacBook-Pro-2.local>
2025-10-22 13:34:01 +0900

    update dashborad file , dashboard to online

--- views/dashboard.php
+++ views/dashboard.php
@@ -13,7 +13,7 @@ $all_users = $user->getAllUsers();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Dashboard online</title>
     <!-- boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Fontawsome -->
